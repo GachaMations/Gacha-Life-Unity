b4e6042dabe60bef52ccc1dc6ae7e9212f9d6e03
GachaMations <gachamations1@gmail.com>
2024-03-09 23:55:02 +0000

    Data Transfer Update

--- Assets/Scripts/DataTransfer.cs
+++ Assets/Scripts/DataTransfer.cs
@@ -1,10 +1,12 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.SharpZipLib.Utils;
 public class DataTransfer : MonoBehaviour
 {
     private static HttpListener listener;
@@ -15,27 +17,44 @@ public class DataTransfer : MonoBehaviour
     {
         while (HostFiles) {
             HttpListenerContext ctx = await listener.GetContextAsync();
-
             HttpListenerRequest req = ctx.Request;
             HttpListenerResponse resp = ctx.Response;
-            //byte[] data = Encoding.UTF8.GetBytes(ZipData);
             resp.ContentEncoding = Encoding.UTF8;
             resp.ContentLength64 = data.LongLength;
+            Debug.Log("user");
             await resp.OutputStream.WriteAsync(data, 0, data.Length);
             resp.Close();
         }
     }
-
+    string SaveDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\GachaUnity";
     public void StartServer()
     {
-        data = File.ReadAllBytes(Application.dataPath+"/resources/TestZip.zip");
+        GameObject.Find("Status").GetComponent<Text>().text = "Zipping Save Files";
+        ZipUtility.CompressFolderToZip(SaveDirectory+"\\Life.zip",null,SaveDirectory+"\\Life");
+        GameObject.Find("Status").GetComponent<Text>().text = "Reading zip's bytes";
+        try {
+            data = File.ReadAllBytes(SaveDirectory+"\\Life.zip");
+        }
+        catch (IOException) {
+            GameObject.Find("Status").GetComponent<Text>().text = "File is bigger than 2gb (somehow)";
+            return;
+        }
+        GameObject.Find("Status").GetComponent<Text>().text = "Starting Server";
         var Port = int.Parse(gameObject.GetComponent<InputField>().text);
         listener = new HttpListener();
         listener.Prefixes.Add("http://localhost:"+Port+"/");
         listener.Start();
         Task t = new Task(Handle);
         t.Start();
-
-        //listener.Close();
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        var privIp = "IP NOT FOUND";
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork) privIp = ip.ToString();
+        }
+        GameObject.Find("Status").GetComponent<Text>().text = "IP: "+privIp+"\nPort: "+Port;
     }
+
+
+
 }
