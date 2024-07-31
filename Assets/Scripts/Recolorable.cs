406a65d6b3f89405b1faf9f8631527b01b0e2ea4
GachaMations <gachamations1@gmail.com>
2024-07-31 18:45:39 +0100

    Tried to get video exporting working. Unfortunately, it turns out that cross-platform image sequence to mp4 conversion isn't very easy :)

--- Assets/Scripts/Recolorable.cs
+++ Assets/Scripts/Recolorable.cs
@@ -6,21 +6,28 @@ public class Recolorable : MonoBehaviour
 {
     public string ColorKey1;
     public string ColorKey2;
+    public bool rawColor;
     SaveFile saves;
     void Awake() {
         saves = GameObject.Find("File").GetComponent<SaveFile>();
     }
     void Update() {
         var currentChar = saves.Load("PlayerData.dat", "CurrentCharacter");
-        var currentColor1 = saves.Load("Characters\\"+currentChar+".oc", ColorKey1);
-        var currentColor2 = saves.Load("Characters\\"+currentChar+".oc", ColorKey2);
-        Color newCol1, newCol2;
-        if (ColorUtility.TryParseHtmlString("#"+currentColor1, out newCol1) && ColorUtility.TryParseHtmlString("#"+currentColor2, out newCol2)) {
-            GetComponent<SpriteRenderer>().material.SetColor("_RemapColor1", newCol1);
-            GetComponent<SpriteRenderer>().material.SetColor("_RemapColor2", newCol2);
-            Debug.Log(newCol1);
+        if (!rawColor) {
+            var currentColor1 = saves.Load("Characters\\"+currentChar+".oc", ColorKey1);
+            var currentColor2 = saves.Load("Characters\\"+currentChar+".oc", ColorKey2);
+            Color newCol1, newCol2;
+            if (ColorUtility.TryParseHtmlString("#"+currentColor1, out newCol1) && ColorUtility.TryParseHtmlString("#"+currentColor2, out newCol2)) {
+                GetComponent<SpriteRenderer>().material.SetColor("_RemapColor1", newCol1);
+                GetComponent<SpriteRenderer>().material.SetColor("_RemapColor2", newCol2);
+                Debug.Log(newCol1);
+            }
         } else {
-            Debug.Log("#"+currentColor1);
+            var currentColor = saves.Load("Characters\\"+currentChar+".oc", ColorKey1);
+            Color newCol;
+            if (ColorUtility.TryParseHtmlString("#"+currentColor, out newCol)) {
+                GetComponent<SpriteRenderer>().material.color = newCol;
+            }
         }
     }
 }
